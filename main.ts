cd41c881ba3c5d9d21f28a6f6fcb0b266bf709dd
clomao <clmao@skgsh.tn.edu.tw>
2022-02-23 10:30:52 +0800

    MakeCode 專案的初始檔案

---
+++ main.ts
@@ -0,0 +1,23 @@
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    girl.startEffect(effects.spray)
+})
+let girl: Sprite = null
+girl = sprites.create(img`
+    . . . . . . . f f . . . . . . . 
+    . . . . . f f 4 4 f f . . . . . 
+    . . . . f 5 4 5 5 4 5 f . . . . 
+    . . . f e 4 5 5 5 5 4 e f . . . 
+    . . f b 3 e 4 4 4 4 e 3 b f . . 
+    . f e 3 3 3 3 3 3 3 3 3 3 e f . 
+    . f 3 3 e b 3 e e 3 b e 3 3 f . 
+    . f b 3 f f e e e e f f 3 b f . 
+    f f b b f b f e e f b f b b f f 
+    f b b b e 1 f 4 4 f 1 e b b b f 
+    . f b b e e 4 4 4 4 4 f b b f . 
+    . . f 4 4 4 e d d d b f e f . . 
+    . . f e 4 4 e d d d d c 4 e . . 
+    . . . f e e d d b d b b f e . . 
+    . . . f f 1 d 1 d 1 1 f f . . . 
+    . . . . . f f f b b f . . . . . 
+    `, SpriteKind.Player)
+controller.moveSprite(girl)
